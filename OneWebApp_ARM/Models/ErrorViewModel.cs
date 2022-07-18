31f2ecc744823a2332167a26a238f2396539fc30
femi-oz <femiosibodu@gmail.com>
2022-07-18 08:19:31 +0100

    Build fix

---
+++ OneWebApp_ARM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OneWebApp_ARM.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
